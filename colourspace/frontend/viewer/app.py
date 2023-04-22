eebacf5f27ec289c656ef84f6eda57bda1e08b44
Svetlin Ankov <svetlin.ankov@live.com>
2023-04-22 21:22:33 +0300

    Rename video_frame into video

--- colourspace/frontend/viewer/app.py
+++ colourspace/frontend/viewer/app.py
@@ -3,7 +3,7 @@
 import wx
 
 from colourspace.av.container import Container
-from colourspace.frontend.window.video_frame import VideoFrame
+from colourspace.frontend.window.video import VideoFrame
 
 
 def main():
