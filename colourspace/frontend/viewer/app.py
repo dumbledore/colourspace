7e061e1137c6599b17a59e949620f3b8d5677de8
Svetlin Ankov <svetlin.ankov@live.com>
2023-04-28 01:45:59 +0300

    Test ColourspaceFilter

--- colourspace/frontend/viewer/app.py
+++ colourspace/frontend/viewer/app.py
@@ -3,6 +3,8 @@
 import wx
 
 from colourspace.av.container import Container
+from colourspace.av.filter import FilteredStream
+from colourspace.av.filter.colourspace import ColourspaceFilter, PROFILES
 from colourspace.frontend.window.video import VideoFrame
 
 
@@ -11,6 +13,9 @@ def main():
 
     container = Container("../data/IMG_5568.MOV")
     stream = container.streams[0]
+
+    filter = ColourspaceFilter(PROFILES["bt2020"], PROFILES["bt709"])
+    stream = FilteredStream(stream, [filter])
     frame = VideoFrame(stream)
     frame.Center()
     frame.Show()
