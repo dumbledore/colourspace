d778e9d2384b88aa45aab2e6d21df950691b9e18
Svetlin Ankov <svetlin.ankov@live.com>
2023-05-02 01:57:38 +0300

    Automatically detect profile from input file

--- colourspace/frontend/viewer/app.py
+++ colourspace/frontend/viewer/app.py
@@ -4,7 +4,7 @@ import wx
 
 from colourspace.av.container import Container
 from colourspace.av.filter import FilteredStream
-from colourspace.av.filter.colourspace import ColourspaceFilter, PROFILES
+from colourspace.av.filter.colourspace import ColourspaceFilter, Profile, PROFILES
 from colourspace.frontend.window.video import VideoFrame
 
 
@@ -14,7 +14,10 @@ def main():
     container = Container("../data/IMG_5568.MOV")
     stream = container.streams[0]
 
-    filter = ColourspaceFilter(PROFILES["bt2020"], PROFILES["bt709"])
+    stream_profile, profile_errors = Profile.from_stream(stream)
+    if profile_errors:
+        print(profile_errors, stream_profile.name)
+    filter = ColourspaceFilter(stream_profile, PROFILES["bt709"])
     stream = FilteredStream(stream, [filter])
     frame = VideoFrame(stream)
     frame.Center()
