33a9d747960ed17b6385161b0b1ee6cb3d2383d5
Svetlin Ankov <svetlin.ankov@live.com>
2023-05-15 02:21:19 +0300

    Remove unused imports from test

--- test/data.py
+++ test/data.py
@@ -1,9 +1,6 @@
 # Copyright (C) 2023, Svetlin Ankov, Simona Dimitrova
 
-import os
-
 from colourspace.util.path import walk_files
-from pymediainfo import MediaInfo
 
 FFMPEG_FATE_SUITE = "../external/FFmpeg/fate-suite"
 FFMPEG_FATE_FILES = walk_files(FFMPEG_FATE_SUITE)
