6a3db2dfe4891cbd4d9db34723c49cb45888afc7
Svetlin Ankov <svetlin.ankov@live.com>
2023-05-17 02:31:36 +0300

    Split the test into multiple cases

--- tests/test_av.py
+++ tests/test_av.py
@@ -6,32 +6,52 @@ from colourspace.av.filter import FilteredStream
 from colourspace.av.filter.colourspace import ColourspaceFilter, Profile, PROFILES
 from colourspace.av.container import Container
 from data import TEST_INFOS
+from PIL import Image
 
 
-def check_stream(stream):
-    stream.frame.to_image()
-    stream.seek(stream.duration / 2)
-    stream.frame.to_image()
+def check_frame(stream):
+    image = stream.frame.to_image()
+
+    assert image, "No frame available"
+    assert type(image) == Image.Image, "Returned frame is wrong type"
+    assert image.size == (stream.width, stream.height), "Wrong frame size"
 
 
 @pytest.mark.parametrize("filename, info", TEST_INFOS.items())
-def test_av(filename, info):
-    container = Container(filename)
+def test_video_frame(filename, info):
+    with Container(filename) as container:
+        for stream in container.streams:
+            # Make sure the frame is valid
+            check_frame(stream)
 
-    for stream in container.streams:
-        check_stream(stream)
 
-    # next test
-    container.seek(0)
+@pytest.mark.parametrize("filename, info", TEST_INFOS.items())
+def test_video_seek(filename, info):
+    with Container(filename) as container:
+        for stream in container.streams:
+            # Seek to half of video
+            stream.seek(stream.duration / 2)
 
-    # now test with ColourSpace
-    for stream in container.streams:
-        is_odd = any([x % 2 for x in (stream.width, stream.height)])
-        if is_odd:
-            # vf_colorspace does not support videos of odd sizes
-            continue
+            # Make sure the frame is valid
+            check_frame(stream)
 
-        stream_profile, _ = Profile.from_stream(stream)
-        filter = ColourspaceFilter(stream_profile, PROFILES["bt709"])
-        stream = FilteredStream(stream, [filter])
-        check_stream(stream)
+
+@pytest.mark.parametrize("filename, info", TEST_INFOS.items())
+def test_video_filter(filename, info):
+    with Container(filename) as container:
+        for stream in container.streams:
+            if any([x % 2 for x in (stream.width, stream.height)]):
+                # vf_colorspace does not support videos of odd sizes
+                continue
+
+            # Get a Profile for this stream
+            stream_profile, _ = Profile.from_stream(stream)
+
+            # Create the filter with default output
+            filter = ColourspaceFilter(stream_profile, PROFILES["bt709"])
+
+            # Create a Stream for this filter
+            stream = FilteredStream(stream, [filter])
+
+            # Make sure the generated frame is valid
+            check_frame(stream)
