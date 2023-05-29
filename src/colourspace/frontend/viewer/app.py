26ea2ba593a62c7d97fe98c17c6bc59f2444d337
Svetlin Ankov <svetlin.ankov@live.com>
2023-05-30 00:55:59 +0300

    A proper App.Open()

--- src/colourspace/frontend/viewer/app.py
+++ src/colourspace/frontend/viewer/app.py
@@ -1,5 +1,6 @@
 # Copyright (C) 2023, Svetlin Ankov, Simona Dimitrova
 
+import os
 import wx
 
 from colourspace.av.container import Container
@@ -13,27 +14,44 @@ class App(wx.App):
         stream = None
 
         if filename:
-            container = Container(filename)
-            stream = container.streams[0]
+            filename = os.path.abspath(filename)
 
-            stream_profile, profile_errors = Profile.from_stream(stream)
-            if profile_errors:
-                print(profile_errors)
-            print(f"Selected input profile: {stream_profile}")
-            filter = ColourspaceFilter(stream_profile, PROFILES["bt709"])
-            stream = FilteredStream(stream, [filter])
+            # Is this already opened?
+            if filename in self._opened:
+                frame = self._opened[filename]
+                frame.Raise()
+                return frame
+
+            # Try opening a new one
+            try:
+                container = Container(filename)
+                stream = container.streams[0]
+
+                stream_profile, profile_errors = Profile.from_stream(stream)
+                if profile_errors:
+                    print(profile_errors)
+                print(f"Selected input profile: {stream_profile}")
+                filter = ColourspaceFilter(stream_profile, PROFILES["bt709"])
+                stream = FilteredStream(stream, [filter])
+            except Exception as e:
+                wx.MessageDialog(None, f"Failed to open '{os.path.basename(filename)}: {e}'",
+                                 "Failed to open file", wx.OK | wx.CENTER | wx.ICON_ERROR).ShowModal()
+                return None
 
         frame = VideoFrame(stream)
         frame.Center()
         frame.Show()
+        self._opened[filename] = frame
+        return frame
 
     def OnInit(self):
-        self.Open("../../data/IMG_5568.MOV")
+        self._opened = {}
         return True
 
 
 def main():
     app = App()
+    app.Open()
     app.MainLoop()
 
 
