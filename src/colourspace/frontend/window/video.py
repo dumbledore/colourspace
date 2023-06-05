1550f0648786751675b983458c0d4a80840f035f
Svetlin Ankov <svetlin.ankov@live.com>
2023-06-06 00:23:53 +0300

    Add menu for viewer

--- src/colourspace/frontend/window/video.py
+++ src/colourspace/frontend/window/video.py
@@ -28,6 +28,9 @@ class VideoFrame(wx.Frame):
             size = self.FromDIP((640, 480))
             self._video = wx.Panel(self, size=size)
 
+        # Initialise the menu
+        self.SetMenuBar(self._create_menu())
+
         # Set current video position in the status bar to 00:00:00
         self._update_video_position(0)
 
@@ -65,3 +68,88 @@ class VideoFrame(wx.Frame):
         duration = time_format(
             self._video.video.duration if self._has_video else 0)
         self._statusbar.SetStatusText(position + "/" + duration)
+
+    def _create_menu(self):
+        menu_bar = wx.MenuBar()
+
+        file_menu = wx.Menu()
+        file_menu.Append(wx.ID_OPEN, "", "Open a video or an image file")
+        file_menu.Append(wx.ID_CLOSE, "", "Close this file")
+        menu_bar.Append(file_menu, "&File")
+
+        edit_menu = wx.Menu()
+        seek_time = edit_menu.Append(wx.ID_ANY, "Seek to time", "Seek to a particular time")
+        seek_frame = edit_menu.Append(wx.ID_ANY, "Seek to frame (slow)", "Seek to a particular frame. Can be very slow")
+        edit_menu.AppendSeparator()
+        edit_menu.Append(wx.ID_SAVE, "", "Save the current frame")
+        menu_bar.Append(edit_menu, "&Edit")
+
+        colourspace = wx.Menu()
+        self.correction_enabled = colourspace.AppendCheckItem(
+            wx.ID_ANY, "Colour Correction", "Enable accurate colour representation")
+        # On by default
+        self.correction_enabled.Check()
+        colourspace.AppendSeparator()
+        input_colourspace = colourspace.Append(wx.ID_ANY, "Input Colourspace", "Select input colourspace")
+        output_colourspace = colourspace.Append(wx.ID_ANY, "Output Colourspace", "Select output colourspace")
+        menu_bar.Append(colourspace, "Colourspace")
+
+        help_menu = wx.Menu()
+        help_menu.Append(wx.ID_ABOUT, "", "About this application")
+        menu_bar.Append(help_menu, "&Help")
+
+        # TODO: Add platform-specific names/shortcuts
+
+        # Bind the events
+        self.Bind(wx.EVT_MENU, self._on_open_file, id=wx.ID_OPEN)
+        self.Bind(wx.EVT_MENU, self._on_close_file, id=wx.ID_CLOSE)
+        self.Bind(wx.EVT_MENU, self._on_seek_time, seek_time)
+        self.Bind(wx.EVT_MENU, self._on_seek_frame, seek_frame)
+        self.Bind(wx.EVT_MENU, self._on_save_frame, id=wx.ID_SAVE)
+        self.Bind(wx.EVT_MENU, self._on_corretion_toggled, self.correction_enabled)
+        self.Bind(wx.EVT_MENU, self._on_input_colourspace, input_colourspace)
+        self.Bind(wx.EVT_MENU, self._on_output_colourspace, output_colourspace)
+        self.Bind(wx.EVT_MENU, self._on_about, id=wx.ID_ABOUT)
+
+        return menu_bar
+
+    # File
+    def _on_open_file(self, event):
+        print("open")
+
+    def _on_close_file(self, event):
+        print("close")
+
+    # Edit
+    def _on_seek_time(self, event):
+        print("seek time")
+
+    def _on_seek_frame(self, event):
+        print("seek frame")
+
+    def _on_save_frame(self, event):
+        print("save")
+
+    # Colourspace
+    def _on_corretion_toggled(self, event):
+        print(f"correction: {self.correction_enabled.IsChecked()}")
+
+    def _on_input_colourspace(self, event):
+        print("input colourspace")
+
+    def _on_output_colourspace(self, event):
+        print("output colourspace")
+
+    # Help
+    def _on_about(self, event):
+        message = """
+Colourspace is a Python library and viewer tool used to render videos and images in the proper colourspace using the colorspace FFmpeg filter.
+
+It uses the av package to access FFmpeg functionality, and pymediainfo to obtain file and stream metadata, that is not yet available through av (even though it is available in FFmpeg).
+
+Note: PyAV uses a (rather) outdated version of FFmpeg: 5.1.2, which may lack support for some newer stuff. Check the FFmpeg build script for PyAV.
+
+Licensed under BSD 3-Clause.
+
+Copyright (C) 2023, Svetlin Ankov, Simona Dimitrova"""
+        wx.MessageDialog(None, message, "About", wx.OK | wx.CENTER | wx.ICON_INFORMATION).ShowModal()
