be62fb54b19c0e08d0b16a4ae1b7afc7d74f4970
Svetlin Ankov <svetlin.ankov@live.com>
2023-06-11 03:55:32 +0300

    Updated the Settings tests

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -24,13 +24,13 @@ def teardown_function():
 
 
 def test_read_one_from_empty():
-    settings = Settings(FILENAME)
+    settings = Settings(FILENAME, 1)
     # Reading from an empty file must return default
     assert settings.get("test1", 123) == 123
 
 
 def test_save_one_read_default():
-    settings = Settings(FILENAME)
+    settings = Settings(FILENAME, 1)
     settings.set("test2", "something")
     # Reading from a non-empty file without the setting
     # must return the default
@@ -38,7 +38,7 @@ def test_save_one_read_default():
 
 
 def test_save_two_read_one():
-    settings = Settings(FILENAME)
+    settings = Settings(FILENAME, 1)
     # Add the setting and some other setting
     settings.set("test1", "something")
     settings.set("test2", "other")
@@ -46,14 +46,24 @@ def test_save_two_read_one():
 
 
 def test_remove_one_from_empty():
-    settings = Settings(FILENAME)
+    settings = Settings(FILENAME, 1)
     settings.remove("test1")
 
 
 def test_save_two_remove_one_get_one_default():
-    settings = Settings(FILENAME)
+    settings = Settings(FILENAME, 1)
     # Add the setting and some other setting
     settings.set("test1", "something")
     settings.set("test2", "other")
     settings.remove("test2")
     assert settings.get("test2", 123) == 123
+
+
+def test_save_two_read_one_wrong_version():
+    settings1 = Settings(FILENAME, 1)
+    # Add the setting and some other setting
+    settings1.set("test1", "something")
+    settings1.set("test2", "other")
+
+    settings2 = Settings(FILENAME, 2)
+    assert settings2.get("test1", 123) == 123
